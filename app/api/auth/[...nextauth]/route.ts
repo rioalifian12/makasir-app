f8773364bb7abc26c18416917a3ac53a9dd51816
rioalifian_12 <rioalifian12@gmail.com>
2025-07-19 02:40:14 +0700

    Memperbaiki auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -43,5 +43,4 @@ export const authOptions: NextAuthOptions = {
 };
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
---
+++ lib/auth.ts
@@ -0,0 +1,12 @@
+// lib/auth.ts
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
